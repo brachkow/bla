3f5536308c7d3fbfa0ec74e24416456332a183c1
Timur Brachkow <33203187+brachkow@users.noreply.github.com>
2025-06-28 13:34:28 +0200

    Fix syntax error

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -177,7 +177,7 @@ const copySessionLink = async () => {
       {{ they || (partnerConnected ? 'Nothing yet...' : 'Waiting for partner to join...') }}
       <div
         class="w-16 h-16 rounded-full absolute top-16 right-16 animate-ping"
-        :class="{partnerConnected ? 'bg-green-500' : 'bg-red-500'}"
+        :class="partnerConnected ? 'bg-green-500' : 'bg-red-500'"
       ></div>
     </div>
     <textarea
